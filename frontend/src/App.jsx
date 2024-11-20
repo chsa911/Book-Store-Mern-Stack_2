2a7db9408921b2194f4d46628e2e08710219dac9
chsa911 <ch.sa911@gmail.com>
2024-11-20 02:53:21 +0100

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CreateBook from './pages/CreateBooks';
 import ShowBook from './pages/ShowBook';
 import EditBook from './pages/EditBook';
 import DeleteBook from './pages/DeleteBook';
+{/*import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './pages/Signup';*/}
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
       <Route path='/books/details/:id' element={<ShowBook />} />
       <Route path='/books/edit/:id' element={<EditBook />} />
       <Route path='/books/delete/:id' element={<DeleteBook />} />
+{ /*     <Route path='/books/signup/' element={<Signup />} />
+      <Route path='/books/login/' element={<Login />} />-**/}
     </Routes>
   );
 };
